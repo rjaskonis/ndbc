bde7eed478777a37bc5ba4a002c7d1a85d839f84
Renne Jaskonis <rjaskonis@gmail.com>
2017-01-12 17:37:22 -0200

    Added blob support (from base64 String); Fixed class Reference to avoid two different 'function calls' share (and mess with) the same variables - e.g.: user1 calls getData setting the 'table' to table1; if at the same time user2 calls getData setting 'table' to table2 for instance, there's risk user1 may get data from table2 instead of table1; BUT it's fixed already

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var db = {
     },
     mssql: {
         north: new ndbc.mssql.Reference({ user: 'SA', password: 'Fly11031507',server: '127.0.0.1', database: 'north', stream:true, connectionTimeout:30000, requestTimeout:30000 }),
-        first: new ndbc.mssql.Reference({ user: 'SA', password: 'Aqua12!',server: 'aquanalyze.no-ip.biz', database: 'first', stream:true, connectionTimeout:30000, requestTimeout:30000 })
+        first: new ndbc.mssql.Reference({ user: 'SA', password: 'Aqua12!',server: '172.16.100.100', database: 'first', stream:true, connectionTimeout:30000, requestTimeout:30000 })
     }
 }
 
@@ -29,14 +29,14 @@ var db = {
 // console.log('** NDBC **');
 
 app.get('/', (req, res) => {
-    // db.mssql.north.table('todos').getData({ where: { 'description like': 'F%' }}).then(people => {
-    //     res.send(people);
+    // db.mysql.aquadoc.table('pessoas').getData().then(people => {
+    //     res.send('people');
     // }).catch(err => {
     //     console.log(err)
     //     res.status(500).json('error');
     // })
 
-    db.mssql.north.table('people').setData({ id:2, name:'Beltrano de tal Filho', birthdate:'1990-01-20', money:0 }).then(response => {
+    db.mysql.north.table('people').setData({ id:5, name:'Renne J', birthdate:'1981-11-20', money:0 }).then(response => {
         console.log(response);
         res.send(':)');
     }).catch(err => {
@@ -45,7 +45,7 @@ app.get('/', (req, res) => {
     })
 
     // PROTOTYPE
-    // db.mysql.north.storedQuery('my-query-name').getData({ parameters: [] }).then(list => {
+    // db.mssql.north.query("SELECT * FROM todos WHERE description like {description}").execute({ description: '%T%'}).then(list => {
     //     res.send(list);
     // }).catch(err => {
     //     console.log(err)
