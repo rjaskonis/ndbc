ec5e534b68455a759c8faf91486f0926d9ce4fde
Renne Jaskonis <rjaskonis@gmail.com>
2017-03-29 11:11:19 -0300

    Fixings

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var ndbc = require('./lib/ndbc');
 var db = {
     mysql: {
         north: new ndbc.mysql.Reference({ host:'127.0.0.1', user:'root', password:'root', database:'north', timezone: 0300 }),
-        aquadoc: new ndbc.mysql.Reference({ host:'aquanalyze.no-ip.biz', user:'root', password:'Uxroot', database:'db_aquadoc', timezone: 0300 })
+        aquadoc: new ndbc.mysql.Reference({ host:'127.0.0.1', user:'root', password:'root', database:'db_aquadoc', timezone: 0300 })
     },
     mssql: {
         north: new ndbc.mssql.Reference({ user: 'SA', password: 'Fly11031507',server: '127.0.0.1', database: 'north', stream:true, connectionTimeout:30000, requestTimeout:30000 }),
@@ -29,6 +29,43 @@ var db = {
 // console.log('** NDBC **');
 
 app.get('/', (req, res) => {
+    // ** MySQL JOIN EXAMPLE **
+    // db.mysql.aquadoc.table('pessoas')
+    // .join({
+    //     type:'inner',
+    //     table: {
+    //         name:'pessoas',
+    //         alias:'fornecedor',
+    //         columns:['id','nome']
+    //     },
+    //         on:[{ foreignColumn:'representantecomercial', column:'id' },{ foreignColumn:'nomecomercial', column:'fornecedor' }]
+    //     })
+    // .join({ type:'inner', table: { name:'pessoas', alias:'representante', columns:['id','nome']}, on:[{ foreignColumn:'representantecomercial', column:'id' }] })
+    // .getData({ where:{ id:1 } }).then(pessoas => {
+    //     console.log(pessoas);
+    // })
+
+    // ** MySQL JOIN EXAMPLE (realistic)**
+    db.mysql.aquadoc.table('pessoas')
+    .join({ table:{ name:'pessoas', alias:'representante', columns:['nome']}, on:[{ foreignColumn:'representantecomercial', column:'id' }] })
+    .getData({
+        columns:['id','nome'],
+        where:{ id:1 }
+    }).then(pessoas => {
+        res.json(pessoas);
+    })
+
+    // ** SQL SERVER JOIN EXAMPLE (realistic)**
+    // db.mssql.north.table('todos')
+    // .join({ table:{ name:'people', alias:'responsible', columns:['name']}, on:[{ foreignColumn:'responsible', column:'id' }] })
+    // .getData({ where:{ 'id not': 0 }}).then(people => {
+    //     res.json(people);
+    // })
+
+    // db.mysql.aquadoc.table('pessoas').getData({ where:{ id:1 } }).then(pessoas => {
+    //     res.json(pessoas);
+    // })
+
     // db.mssql.first.table('SA1').getData({ where:{ A1_NOME:'RENNE JASKONIS' } }).then(people => {
     //     console.log(people);
     //     res.send('people');
@@ -54,7 +91,7 @@ app.get('/', (req, res) => {
     // })
 
     // PROTOTYPE
-    // db.mssql.north.query("SELECT * FROM todos WHERE description like {description}").execute({ description: '%T%'}).then(list => {
+    // db.mysql.north.query("select p.*, 1 as 'p.teste' from people p").execute({ description: '%T%'}).then(list => {
     //     res.send(list);
     // }).catch(err => {
     //     console.log(err)
